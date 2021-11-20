2ceccdbec44da904599f8c74480f80ac179e6aad
austi <austi@DESKTOP-20645UC>
2021-11-19 19:26:48 -0800

    Add project files.

---
+++ ProjectOne.cs/Game.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne.cs
+{
+    class Game
+    {
+        public int lives;
+        public string hidden;
+        
+        public Game(int lives, string hidden)
+        {
+            lives = this.lives;
+            hidden = this.hidden;
+           
+        }
+
+        public static bool PlayGame(int lives, string hidden)
+        {
+            int numGeuss = 0;
+            string[] secret = new string[hidden.Length];
+            bool gameWon = false;
+            LetterSpots(hidden, secret, numGeuss);
+            while (gameWon == false)
+            {
+                
+                Console.Write("guess a letter");
+                string guess = Console.ReadLine();
+                Console.WriteLine(guess);
+                secret = GuessChecker(guess,hidden, secret);
+                LetterSpots(hidden, secret, numGeuss);
+                if (DidYaWin(hidden, secret) == true)
+                {
+                    gameWon = true;
+                }
+
+
+                numGeuss++;
+            }
+
+
+            return true;
+
+
+        }
+
+        public static bool DidYaWin(string hidden,string[] secret)
+        {
+            Console.Write("secrete == " + secret[0]);
+            string checking = "";
+            for (int i = 0; i < hidden.Length; i++)
+            {
+                //string p = secret[0]; 
+                
+                checking = checking + secret[i];
+
+            }
+            if (checking == hidden)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static void LetterSpots(string hidden, string[] secret, int numGuess)
+        {
+            if (numGuess == 0) // if num guess == 0 then do this <<<<<<<<
+            {
+                Console.Write("\t\t");
+                for (int i = 0; i < hidden.Length; i++)
+                {
+                    secret[i] = "_ "; 
+                    Console.Write("_ ");
+                }
+            }
+            else
+            {
+                Console.Write("\t\t");
+                for (int j = 0; j < hidden.Length; j++)
+                {
+                    Console.Write(secret[j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        public  static  string[] GuessChecker(string guess, string hidden, string[] secret) 
+        {
+            Console.WriteLine(hidden);
+            
+            for (int i = 0; i < hidden.Length; i++)
+            {
+                if (guess[0] == hidden[i])
+                {
+                    secret[i] = guess;
+                    
+                }
+                
+                
+                
+            }
+            return secret;
+
+
+        }
+        public void GettingStarted()
+        {
+            // print what game is, game rules, ect 
+
+        }
+
+
+
+    }
+    // >=a && <=z <<<<<<<< add later in guesschecker. 
+}
---
+++ ProjectOne.cs/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProjectOne.cs
+{
+    
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            bool runGame = true;
+            int lives = 3;
+            string hidden;
+
+            Random random = new Random();
+            string[] randomWord = new string[10];
+
+            StreamReader reading = new StreamReader("file1.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                randomWord[i] = reading.ReadLine(); 
+            }
+            reading.Close();
+
+            int n = random.Next(10);
+            Console.WriteLine(n);
+            
+            while (runGame == true)
+            {
+                Console.WriteLine("would you like to play a game");
+
+                if (Console.ReadLine() == "n")
+                {
+                    break;
+                }
+
+                // move random numbers here so string changes when you start new game.
+                Game game1 = new Game(lives, randomWord[n]);
+                if(Game.PlayGame(lives, randomWord[n])== true)
+                {
+                    Console.WriteLine("you won");
+                }
+              
+                
+
+
+            }
+
+
+
+
+
+        }
+
+    }
+}
+// NEED TO DO: 1) create and implament Getting started method  >>>>>  2) move Random() num so string changes when new game begins >>>>>>
+// >>>>>>>>> 3) desperatly need to make the visual elements look nice  >>>>>> 4) need to add function when user inputs more than 1 char to restart pick a number.
+// >>>>>>> 5) also function needs to make sure the char input is a letter not num or other (&*%^$#). num guess = 0 causes error if you gaues first case corectly 
+// >>>>>>> 6) since time will most lickely allow add a player class that holds player data. 
